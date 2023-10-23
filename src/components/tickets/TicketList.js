a370358dd72bcf012163f63fe2162bdd1eec67e1
Colin Matteson <seefm@proton.me>
2023-10-23 15:10:42 -0400

    minor fix to clear key related errors during JSX rendering

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -39,7 +39,7 @@ export const TicketList = () => {
         {
             filteredTickets.map(
                 (ticket) => {
-                    return <section className="ticket">
+                    return <section className="ticket" key={`ticket--${ticket.id}`}>
                         <header>{ticket.description}</header>
                         <footer>Emergency: {ticket.emergency ? "Yes" : "No"}</footer>
                     </section>
